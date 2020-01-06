24b33ec326b61724573576989a9fd02cf5a44c40
npdarsini <pn535@mail.umkc.edu>
2020-01-05 21:32:35 -0500

    Success and Warning Components

--- src/app/success/success.component.ts
+++ src/app/success/success.component.ts
@@ -3,8 +3,16 @@ import {Component} from "@angular/core";
 
 @Component({
   selector: 'success',
-  templateUrl: './success.component.html',
-  styleUrls: ['./success.component.css']
+  template: `<h3>You are Successful!!</h3>`,
+  //styleUrls: ['./success.component.css']
+  styles: [`
+    h3 {
+      padding: 20px;
+      background-color: darkseagreen;
+      border: 1px solid darkgreen;
+    }
+
+  `]
 })
 export class SuccessComponent {
 
